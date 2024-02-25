cd92b120091d5fa635de03c9faef39b3a060320a
jjvalkema <jjvalkema@protonmail.com>
2024-02-25 18:56:23 +0100

    added harpie

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -4,39 +4,27 @@ import { heading, panel, text } from '@metamask/snaps-ui';
 // Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
 
-  // Use the window.ethereum global provider to fetch the gas price.
-  const currentGasPrice = await window.ethereum.request({
-    method: 'eth_gasPrice',
-  });
-
-  // Get fields from the transaction object.
-  const transactionGas = parseInt(transaction.gas as string, 16);
-  const currentGasPriceInWei = parseInt(currentGasPrice ?? '', 16);
-  const maxFeePerGasInWei = parseInt(transaction.maxFeePerGas as string, 16);
-  const maxPriorityFeePerGasInWei = parseInt(
-    transaction.maxPriorityFeePerGas as string,
-    16,
-  );
-
-  // Calculate gas fees the user would pay.
-  const gasFees = Math.min(
-    maxFeePerGasInWei * transactionGas,
-    (currentGasPriceInWei + maxPriorityFeePerGasInWei) * transactionGas,
-  );
-
-  // Calculate gas fees as percentage of transaction.
-  const transactionValueInWei = parseInt(transaction.value as string, 16);
-  const gasFeesPercentage = (gasFees / (gasFees + transactionValueInWei)) * 100;
+  const harpieFetch = await fetch("https://api.harpie.io/v2/validateTransaction", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: String(JSON.stringify({
+      apiKey: "d8832f15-e9e5-489c-9087-bea84a79258a",
+      to: transaction.to,
+      value: transaction.value,
+      from: transaction.from,
+      data: transaction.data
+    }))
+  })
+  const harpieResult = await harpieFetch.json()
 
   // Display percentage of gas fees in the transaction insights UI.
   return {
     content: panel([
-      heading('Transaction insights Snap'),
-      text(
-        `As set up, you are paying **${gasFeesPercentage.toFixed(
-          2,
-        )}%** in gas fees for this transaction.`,
-      ),
+      heading(String(harpieResult['summary'])), 
+      text(String(JSON.stringify(harpieResult))),
     ]),
   };
 };

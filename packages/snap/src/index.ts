c544881b9daef41967d62fefead32b3ad7cc9276
jjvalkema <jjvalkema@protonmail.com>
2024-02-25 19:28:54 +0100

    made a function to get contract name and the transaction info

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,16 +1,31 @@
 import { OnTransactionHandler } from '@metamask/snaps-types';
 import { heading, panel, text } from '@metamask/snaps-ui';
+const harpieApiKey = "d8832f15-e9e5-489c-9087-bea84a79258a"
 
-// Handle outgoing transactions.
-export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
-  let txData 
+async function getContractName(address: String) {
+  const harpieFetchResult = await fetch("https://api.harpie.io/v2/getcontractname", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      apiKey: harpieApiKey,
+      address: address
+    })
+  })
+  return (await harpieFetchResult.json())["contractOwner"]
+}
+
+async function getTransactionInformation(transaction: any) {
+  let txData
   if (transaction.data) {
-    txData=transaction.data
+    txData = transaction.data
   } else {
     txData = "0x0"
   }
 
-  const harpieFetch = await fetch("https://api.harpie.io/v2/validateTransaction", {
+  const harpieFetchResult = await fetch("https://api.harpie.io/v2/validateTransaction", {
 
     method: "POST",
     headers: {
@@ -18,20 +33,29 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
       'Content-Type': 'application/json'
     },
     body: String(JSON.stringify({
-      apiKey: "d8832f15-e9e5-489c-9087-bea84a79258a",
+      apiKey: harpieApiKey,
       to: transaction.to,
       value: transaction.value,
       from: transaction.from,
       data: txData
     }))
   })
-  const harpieResult = await harpieFetch.json()
+  return await harpieFetchResult.json()
+}
+
+// Handle outgoing transactions.
+export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+  const harpieTransactionInformation = await getTransactionInformation(transaction)
+  const contractName = await getContractName(String(transaction.to))
+
+
 
   // Display percentage of gas fees in the transaction insights UI.
   return {
     content: panel([
-      heading(String(harpieResult['summary'])), 
-      text(String(JSON.stringify(harpieResult))),
+      heading(String(harpieTransactionInformation['summary'])),
+      text(`Sending transaction to ${contractName}`),
+      text(String(JSON.stringify(harpieTransactionInformation))),
     ]),
   };
 };

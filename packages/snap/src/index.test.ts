0436bba8208b7cb3fe0fc906796a6367fedd47ac
aliza13 <alizaajc13@gmail.com>
2024-02-27 17:27:47 -0700

    sourcify functions implemented correctly

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -1,7 +1,8 @@
 import { installSnap } from '@metamask/snaps-jest';
 import { panel, text, heading } from '@metamask/snaps-sdk';
 import {harpieFunctions} from "./harpieFunctions"
-import { ethers } from 'ethers';
+import { SourcifyFunctions } from './sourcifyFunctions';
+
 
 describe('happyHarpie', () => {
   it('should return insight of a normal eth transfer to the 3pool contract', async () => {
@@ -29,12 +30,23 @@ describe('happyHarpie', () => {
 
     const harpieTransactionInformation = await harpieFunctions.createInformationMessage(transaction)
     
+    let harpieContractYesNo = await harpieFunctions.isContract(transaction["to"]);
+    console.log("This is the bool from isContract: ", harpieContractYesNo);
+    let sourcifyTxInfo:string = "You are not interacting with a smart contract";
+    // console.log(typeof(sourcifyTxInfo), "This is STxIn", sourcifyTxInfo);
+
+    if (harpieContractYesNo === true) {
+      sourcifyTxInfo = await SourcifyFunctions.evaluation(transaction.to);
+      // console.log(typeof(sourcifyTxInfo), "This is STxIn", sourcifyTxInfo);
+    }
+
     //checks if the info rendered matches that with what onTransaction(transaction) made from simulating
     //this is from jest https://jestjs.io/docs/getting-started
     expect(response).toRender(
       panel([
         heading(harpieTransactionInformation.header),
-        text(harpieTransactionInformation.body)
+        text(harpieTransactionInformation.body),
+        text(sourcifyTxInfo)
       ]),
       );
   });
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -9,12 +9,19 @@ import { SourcifyFunctions } from './sourcifyFunctions';
 //the function needs to called onTransaction otherwise metamask doesnt see it
 //https://docs.metamask.io/snaps/reference/entry-points/#ontransaction
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
-  console.log(transaction)
+  console.log("the tx:",transaction)
 
   //get the infornmation from harpie about the tx
   const harpieTransactionInformation = await harpieFunctions.createInformationMessage(transaction)
   console.log(`tx info from harpie: ${String(JSON.stringify(harpieTransactionInformation))}`)
-  const sourcifyTxInfo = await SourcifyFunctions.evaluation(transaction.to);
+
+  // need to check if interaction is w a contract not an EOA, if so run this, if not skip
+  let harpieContractYesNo = await harpieFunctions.isContract(transaction["to"]);
+  let sourcifyTxInfo:string = "You are not interacting with a smart contract";
+
+  if (harpieContractYesNo === true) {
+    sourcifyTxInfo = await SourcifyFunctions.evaluation(transaction.to);
+  }
 
   //displays the info gathered
   //panel, heading and text are from metasmask snap api:
@@ -23,7 +30,7 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
     content: panel([
       heading(harpieTransactionInformation.header),
       text(harpieTransactionInformation.body),
-      heading(sourcifyTxInfo)
+      text(sourcifyTxInfo),
     ]),
   };
 };
